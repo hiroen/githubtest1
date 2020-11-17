9b90a63717cce46e4ed31fbae3863296708cbe5c
hiroen <hiroen69@gmail.com>
2020-11-17 14:23:53 +0900

    今度こそのコミット

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static voic main(String[] args){
+    public static void main(String[] args){
         System.out.println("初めてのGitHubです");
         System.out.println("テストテストテスト");
     }
