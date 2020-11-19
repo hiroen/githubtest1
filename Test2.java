26dc4c6c6597a134abf87158fb34fcf9e8f6ebb1
hiroen <hiroen69@gmail.com>
2020-11-19 17:45:10 +0900

    謎

--- Test2.java
+++ Test2.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Test2 {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int[] line = new int[4];
         for(int i = 0;i < line.length;i++){
