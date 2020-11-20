ba502a262387a302107a0fc4ac7ea719ad8cf7df
hiroen <hiroen69@gmail.com>
2020-11-20 15:04:01 +0900

    修正しました

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args){
         System.out.println("初めてのGitHubです");
         System.out.println("テストテストテスト");
         Test name1 = new Test(27,"Hiroe");
         name1.getName();
+
     }
 }
