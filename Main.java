373e1cdbddda919a141a6ea95b6f2f8b87df634e
hiroen <hiroen69@gmail.com>
2021-02-02 11:22:36 +0900

    テストコミット

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-import java.util.*;
-public class Main {
+public class Main{
     public static void main(String[] args){
-        System.out.println("初めてのGitHubです");
-        System.out.println("テストテストテスト");
-        Test name1 = new Test(27,"Hiroe");
-        name1.getName();
-
+        System.out.println("Hello!");
     }
 }
